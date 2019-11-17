f21e6e2e42d57190c06a1e2b27cedc4ffbd34024
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-11-16 21:14:47 -0500

    "Add to cart" button added

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -38,4 +38,3 @@ namespace SportsStore.Controllers
         });
     }
 }
-// defining cart model
---
+++ SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        // this will bring back to previous page to continue shopping 
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue
+            // if has value
+            ? $"{request.Path}{request.QueryString}"
+            // if has no value
+            : request.Path.ToString();
+    }
+}
