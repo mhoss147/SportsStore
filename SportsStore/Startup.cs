73613e3158c21d76991ebfbcb12a70482671c464
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-11-16 21:20:41 -0500

    Session enabled in the memory

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -26,6 +26,10 @@ namespace SportsStore
             options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            // allocating session in the memory
+            services.AddMemoryCache();
+            services.AddSession();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +41,7 @@ namespace SportsStore
             }
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
