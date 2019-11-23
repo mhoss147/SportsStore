1f9411a129d01936edaca0d3be0933235bae8ebc
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-11-23 08:16:05 -0500

    Service registered

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -30,6 +30,9 @@ namespace SportsStore
             services.AddMemoryCache();
             services.AddSession();
 
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
