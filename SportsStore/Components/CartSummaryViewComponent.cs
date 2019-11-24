b0223e1fb309e23efb1d068dff24d2ccdcb0bac8
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-11-23 19:06:55 -0500

    ShoppingCart view at the right corner creadted

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        // constructor
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        // method
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+
+    }
+}
